3f397e5e2a5fbd139b1fc04ffce6144f28296460
LanchevychMaxym <lanchevich@gmail.com>
2025-01-28 22:54:22 +0200

    started work on traceability

---
+++ OpenTelemetry.Shared/Traceability/Traceable.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace OpenTelemetry.Shared.Traceability
+{
+    public class Traceable<TType>
+        where TType : class
+    {
+        internal const string ActivitySourceName = nameof(TType);
+        public static ActivitySource ActivitySource { get; } = new ActivitySource(ActivitySourceName);
+
+        public TResult TraceAction<TResult>(Func<TResult> action)
+        {
+            using (var activity = ActivitySource.StartActivity(action.Method.Name))
+            {
+                TResult result;
+
+                try
+                {
+                    result = action();
+                    activity?.SetTag($"{action.Method.Name}.Result", result);
+                }
+                catch (Exception ex)
+                {
+                    activity?.SetStatus(ActivityStatusCode.Error, ex.Message);
+                    throw;
+                }
+
+                return result;
+            }
+        }
+        public void TraceAction(Action action)
+        {
+            using (var activity = ActivitySource.StartActivity(action.Method.Name))
+            {
+                try
+                {
+                    action();
+                }
+                catch (Exception ex)
+                {
+                    activity?.SetStatus(ActivityStatusCode.Error, ex.Message);
+                    throw;
+                }
+            }
+        }
+    }
+}
